b37f652742eafdf2ce6639d280a717430f905550
Hironobu Nishikokura <nishiko@mindia.jp>
2015-09-23 16:30:05 +0900

    銀行残高の計算ロジック見直し

--- app/models/hareruya_freee/month.rb
+++ app/models/hareruya_freee/month.rb
@@ -1,5 +1,18 @@
 module HareruyaFreee
   class Month < ::Month
+
+    def self.refresh!
+      self.order('id asc').each do |month|
+        month.refresh!
+      end
+    end
+
+    def refresh!
+      self.income(true)
+      self.expense(true)
+      self.balance
+    end
+
     def incomes
       WalletTxn.where(entry_side: 'income', date: first_date..last_date)
     end
@@ -24,14 +37,36 @@ module HareruyaFreee
       end
     end
 
-    def expense
-      cols && cols[:expense] ? cols[:expense] : nil
+    def expense refresh = false
+      if !refresh && cols && cols[:expense]
+        cols[:expense]
+      else
+        self.cols ||= {}
+        if !this_month? && future?
+          res = Secretdata.expenses.values
+        else
+          res = expenses.map{|d| d.amount }
+        end
+        self.cols[:expense] = res.reduce(:+) || 0
+        self.save!
+        self.cols[:expense]
+      end
     end
 
     def balance
-      res = [4409, 4411].map{|walletable_id|  # SBIと三井住友
-        WalletTxn.where(entry_side: 'expense', date: first_date..last_date, hareruya_freee_walletable_id: walletable_id).last.balance
-      }.reduce(:+)
+      if future?
+        if prev
+          res = prev.balance + profit
+        else
+          res = 0
+        end
+      else
+        res = [4409, 4411].map{|walletable_id|  # SBIと三井住友
+          txns = WalletTxn.where(entry_side: 'expense', date: first_date..last_date, hareruya_freee_walletable_id: walletable_id)
+          txns.present? ? txns.last.balance : 0
+        }.reduce(:+)
+
+      end
       self.cols[:balance] = res
       self.save!
       res
@@ -39,14 +74,6 @@ module HareruyaFreee
 
     def profit
       (income || 0 ) - (expense || 0)
-    end
-
-    def fetch_balance
-      if future?
-        prev.balance + profit
-      else
-        balance
-      end
     end
 
     def self.sync
@@ -59,7 +86,7 @@ module HareruyaFreee
         cols[:profit] = (cols[:income] || 0 ) - (cols[:expense] || 0)
 
         cols[:profit] = cols[:income] - cols[:expense] if month.future?
-        cols[:balance] = month.fetch_balance
+        cols[:balance] = month.balance
 
         month.cols = cols
 
